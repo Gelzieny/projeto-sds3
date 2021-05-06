181ba4c9355e0a56af2e63ab2dea35b16fbe0c2f
Gelzieny R. Martins <gelzieny@gmail.com>
2021-05-06 17:18:44 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,17 @@
-import React from 'react';
+import Navbar from "components/Navbar";
+import Footer from "components/Footer";
+import DataTable from "components/DataTable";
 
 function App() {
   return (
-    <h2 className="text-primary">Olá Mundo!!</h2>
+    <>
+      <Navbar/>
+      <div className="container">
+        <h2 className="text-primary">Olá Mundo!!</h2>
+        <DataTable/>
+      </div>  
+      <Footer/>
+    </>
   );
 }
 
