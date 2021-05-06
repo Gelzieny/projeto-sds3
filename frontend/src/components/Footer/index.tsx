5ef3b1e3e0b02b76e686a6533a8e7a2aa811cdbc
Gelzieny R. Martins <gelzieny@gmail.com>
2021-05-06 18:46:00 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,20 @@
+const Footer = () => {
+  return (
+    <footer className="footer mt-auto py-3 bg-dark">
+      <div className="container">
+        <p className="text-light">
+          App desenvolvido por{" "}
+          <a
+            href="https://github.com/gelzieny"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Gelzieny R. Martins
+          </a>
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
