77caa36b4fb131c90069aea09bfa782260d06a2e
Gelzieny R. Martins <gelzieny@gmail.com>
2021-05-06 22:12:59 -0300

    Group by search

--- backend/src/main/java/com/devsuperior/dsvendas/service/SaleService.java
+++ backend/src/main/java/com/devsuperior/dsvendas/service/SaleService.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.dsvendas.dto.SaleDTO;
+import com.devsuperior.dsvendas.dto.SaleSuccessDTO;
 import com.devsuperior.dsvendas.dto.SaleSumDTO;
 import com.devsuperior.dsvendas.entities.Sale;
 import com.devsuperior.dsvendas.repositories.SaleRepository;
@@ -33,5 +34,9 @@ public class SaleService {
 	public List<SaleSumDTO> amountGroupedBySeller(){
 		return saleRepository.amountGroupedBySeller();
 	}
-
+	
+	@Transactional(readOnly = true)
+	public List<SaleSuccessDTO> successGroupedBySeller(){
+		return saleRepository.successGroupedBySeller();
+	}
 }
