a6d9fcfaf0899392d9d0d8b84465b894227c4d33
Gelzieny R. Martins <gelzieny@gmail.com>
2021-05-08 22:02:36 -0300

    Correção

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -12,6 +12,20 @@ const Footer = () => {
             Gelzieny R. Martins
           </a>
         </p>
+        <p className="text-light">
+          <small>
+            <strong>Semana Spring React</strong>
+            <br />
+            Evento promovido pela escola DevSuperior:{" "}
+            <a
+              href="https://instagram.com/devsuperior.ig"
+              target="_blank"
+              rel="noreferrer"
+            >
+              @devsuperior.ig
+            </a>
+          </small>
+        </p>
       </div>
     </footer>
   );
